c4378bb7e85b540e9e9502349a6dfdb1ba70ea8d
Blin <bk51836@ubt-uni.net>
2024-05-22 21:33:44 +0200

    using javascript effect in index page

--- main.js
+++ main.js
@@ -3,3 +3,35 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active')
 }
+
+
+function typeEffect(element, speed) {
+    const text = element.innerHTML;
+    element.innerHTML = '';
+
+    let i = 0;
+    const interval = setInterval(function() {
+        if (i < text.length) {
+            if (text.charAt(i) === '<') {
+                const endIndex = text.indexOf('>', i) + 1;
+                
+                element.innerHTML += text.substring(i, endIndex);
+                i = endIndex;
+            } else {
+                element.append(text.charAt(i));
+                i++;
+            }
+        } else {
+            clearInterval(interval);
+        }
+    }, speed);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    AOS.init();
+
+    const textElement = document.querySelector('.home-text h1');
+
+    typeEffect(textElement, 100); 
+});
